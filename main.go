01bc1c393cb5e11d8f9420c6cca058114533dece
caneatcansleep <1404688903@qq.com>
2024-01-28 09:22:35 -0800

    test

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"log"
 	"net"
+	"node/services"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"unsafe"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
+
+var ListenToLeftAddr = "localhost:8082"
+var ListenToControllerAddr = "localhost:8081"
+var controllerAddr = "localhost:8080"
+var connCh chan net.Conn
+
+const digits = "0123456789abcdef"
 
 type IP []byte
 
@@ -26,20 +40,40 @@ type TcpAddr struct {
 	Port int
 }
 
-var connCh chan net.Conn
-
 func init() {
 	connCh = make(chan net.Conn)
 }
 
-func (ip IP) String() string {
-	tmp := make([]byte, 7)
-	for i := 0; i < 3; i++ {
-		tmp = append(tmp, ip[i])
-		tmp = append(tmp, '.')
+func (ip IP) string4() string {
+	const max = len("255.255.255.255")
+	ret := make([]byte, 0, max)
+	ret = ip.appendTo4(ret)
+	return string(ret)
+}
+
+func (ip IP) appendTo4(ret []byte) []byte {
+	ret = appendDecimal(ret, ip[0])
+	ret = append(ret, '.')
+	ret = appendDecimal(ret, ip[1])
+	ret = append(ret, '.')
+	ret = appendDecimal(ret, ip[2])
+	ret = append(ret, '.')
+	ret = appendDecimal(ret, ip[3])
+	return ret
+}
+
+// appendDecimal appends the decimal string representation of x to b.
+func appendDecimal(b []byte, x uint8) []byte {
+	// Using this function rather than strconv.AppendUint makes IPv4
+	// string building 2x faster.
+
+	if x >= 100 {
+		b = append(b, digits[x/100])
 	}
-	tmp = append(tmp, ip[3])
-	return string(tmp)
+	if x >= 10 {
+		b = append(b, digits[x/10%10])
+	}
+	return append(b, digits[x%10])
 }
 
 func ConnectToNextHop(relayInfo RelayInfo) {
@@ -47,7 +81,7 @@ func ConnectToNextHop(relayInfo RelayInfo) {
 	var rightConn net.Conn
 	var err error
 
-	nextHop := relayInfo.NextHop.String() + strconv.Itoa(relayInfo.NextHopPort)
+	nextHop := relayInfo.NextHop.string4() + strconv.Itoa(relayInfo.NextHopPort)
 	fmt.Println("nextHop = ", nextHop)
 
 	// 有可能下一跳还没有监听，因此我们需要不断尝试建立tcp连接，直到建立成功
@@ -110,8 +144,8 @@ func ProcessMsgFromController(connController net.Conn) {
 		return
 	}
 	// 这里可能有问题，关键是Sizeof函数的作用可能不是我理解的那样
-	if n < int(unsafe.Sizeof(RelayInfo{})) {
-		fmt.Printf("read %d byte from controller, less than sizeof(RelayInfo)\n", n)
+	if n < 12 {
+		log.Fatalf("read %d byte from controller, less than 12 bytes!\n", n)
 		return
 	}
 	relayInfo := RelayInfo{}
@@ -124,12 +158,12 @@ func ProcessMsgFromController(connController net.Conn) {
 }
 
 func ListenToController() {
-	listen, err := net.Listen("tcp", "192.168.19.136:8080")
+	listen, err := net.Listen("tcp", ListenToControllerAddr)
 	if err != nil {
-		fmt.Println("ListenToController: Listen() failed, error = ", err)
+		log.Fatalln("ListenToController: Listen() failed, error = ", err)
 		return
 	} else {
-		fmt.Println("ListenToController: Listen to controller success!")
+		log.Println("ListenToController: Listen to controller success!")
 	}
 
 	for {
@@ -145,10 +179,10 @@ func ListenToController() {
 func ListenToLeft(address string) {
 	listen, err := net.Listen("tcp", address)
 	if err != nil {
-		fmt.Printf("Listen at %s failed, error = %v\n", address, err)
+		log.Fatalf("Listen at %s failed, error = %v\n", address, err)
 		return
 	} else {
-		fmt.Printf("Listen at %s, wait pre hop to connect us.\n", address)
+		log.Printf("Listen at %s, wait pre hop to connect us.\n", address)
 	}
 
 	for {
@@ -162,8 +196,51 @@ func ListenToLeft(address string) {
 	}
 }
 
+func registerNode() {
+	//连接server端
+	timeout := 5 * time.Minute
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	conn, err := grpc.DialContext(ctx, controllerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("grpc.Dial err")
+	}
+	defer conn.Close()
+
+	c := services.NewControllerClient(conn)
+	defer cancel()
+	resp, err := c.RegisterNode(ctx, &services.RegisterNodeRequest{Ip: ipStringToInt("localhost")})
+	if err != nil {
+		log.Printf("failed to call RegisterNode, error = %v\n", err)
+		return
+	}
+	fmt.Printf("register node, get node id = %d\n", resp.NodeId)
+}
+
+func ipStringToInt(ip string) int32 {
+	if ip == "localhost" {
+		ip = "127.0.0.1"
+	}
+	strs := strings.Split(ip, ".")
+	if len(strs) != 4 {
+		panic("wrong ip fromat!")
+	}
+	tmp := make([]byte, 0, 4)
+	for i := 0; i < 4; i++ {
+		v, err := strconv.Atoi(strs[i])
+		if err != nil {
+			panic("failed to call strconv.Atoi")
+		}
+		tmp = append(tmp, byte(v))
+	}
+	// 对于localhost而言，返回的就是0x100007f
+	return int32(binary.LittleEndian.Uint32(tmp))
+
+}
 func main() {
+	fmt.Printf("%x\n", ipStringToInt("localhost"))
+	fmt.Println(int(unsafe.Sizeof(RelayInfo{})))
+	registerNode()
 	go ListenToController()
-	address := "xx.xx.xx.xx:8080"
-	ListenToLeft(address)
+	ListenToLeft(ListenToLeftAddr)
+
 }
